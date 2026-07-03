6748e75acb03d23a3b07ae35a2f04520cc7100dc
sunqi <zqbysq@163.com>
2026-07-03 14:26:37 +0800

    perf: [updateManager] 发送消息记录到日志

--- src/main/modules/update/WindowsUpdater.js
+++ src/main/modules/update/WindowsUpdater.js
@@ -55,6 +55,7 @@ export default class WindowsUpdater extends BaseUpdater {
  */
   sendEvent(channel, data = {}) {
     if (this.mainWindow && !this.mainWindow.isDestroyed()) {
+      log.info(`[WindowsUpdater] Sending event to ${channel} Start!`)
       this.mainWindow.webContents.send(channel, data)
     }else{
       log.error('[WindowsUpdater] 主窗口已销毁，无法发送事件',channel,data)
