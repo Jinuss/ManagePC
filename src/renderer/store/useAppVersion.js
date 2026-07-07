3b9c5d42b454798bd20a990442e38f474d434118
sunqi <zqbysq@163.com>
2026-07-07 15:18:33 +0800

    fix:  设置pinia中的hasUpdate初始化

--- src/renderer/store/useAppVersion.js
+++ src/renderer/store/useAppVersion.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useAppVersionStore = defineStore("appVersion", {
   state: () => ({
-    hasUpdate: true,
+    hasUpdate: false,
   }),
   actions: {
     setHasUpdate(hasUpdate) {
